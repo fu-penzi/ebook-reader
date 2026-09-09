c47a3015f800efab4bb994bf6246da8917be1491
Piotr Nowak <npiotrek44@gmail.com>
2026-09-09 22:27:45 +0200

    feat: Add media controls to PC

--- apps/web/src/routes/b/+page.svelte
+++ apps/web/src/routes/b/+page.svelte
@@ -1149,7 +1149,7 @@
     ttsController?.stop({ keepPosition: true });
   }
 
-  $: showTtsBar = $isMobile$ && ttsSpeaking;
+  $: showTtsBar = ttsSpeaking;
   $: ttsBarOffset = showTtsBar ? 'calc(3.5rem + env(safe-area-inset-bottom, 0px))' : '0px';
 
   function restartTts() {
